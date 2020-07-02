7f33459dd3275df749d5fb8ce37e3330a8622444
hungdt <hungdt>
2020-07-03 03:22:50 +0700

    Updates

---
+++ precache-manifest.fe7c873e320e952b70694fb4f5f1fef0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897633fc2cf30f0cd28b539060b730f1",
+    "url": "/some-demo/index.html"
+  },
+  {
+    "revision": "bddf7a49fa4da446f947",
+    "url": "/some-demo/static/js/2.b1584ad2.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/some-demo/static/js/2.b1584ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c0941c6a7d966e5adb",
+    "url": "/some-demo/static/js/main.c225bfd5.chunk.js"
+  },
+  {
+    "revision": "22bb7fb22685761b8d06",
+    "url": "/some-demo/static/js/runtime-main.bdeb54d2.js"
+  },
+  {
+    "revision": "8872ea462815122772e598ea0f54f281",
+    "url": "/some-demo/static/media/airbnb.8872ea46.svg"
+  },
+  {
+    "revision": "aee9f5072f7c7c3d25323c71a213a9e9",
+    "url": "/some-demo/static/media/background.aee9f507.jpg"
+  },
+  {
+    "revision": "f8a1a2e5ffc7f0cd7bd591885b74e58b",
+    "url": "/some-demo/static/media/night-icon.f8a1a2e5.svg"
+  },
+  {
+    "revision": "02f11d1f91bda442b86a5763de986296",
+    "url": "/some-demo/static/media/sunny-icon.02f11d1f.svg"
+  }
+]);
