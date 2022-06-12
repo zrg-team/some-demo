7403e45db76c1c5fadbee2e489093fd50fc83d31
Hung Duong <hung.duong@MB049.local>
2022-06-12 21:23:05 +0700

    feat: add getData

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,10 +4,14 @@ import Header from '../common/containers/Header'
 import SearchForm from '../modules/demo/containers/SearchForm'
 
 function HomePage () {
+  const getData = (input) => {
+    return input?.length
+  }
   return (
     <Page>
       <Header />
       <SearchForm />
+      {getData('hung')}
     </Page>
   )
 }
