bb2263fa8b58c05afdd343a05d2b14a5819b0f26
Hung Duong <hung.duong@MB049.local>
2022-06-20 10:20:40 +0700

    feat: export-1

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import Page from '../common/hocs/Page'
 import Header from '../common/containers/Header'
 import SearchForm from '../modules/demo/containers/SearchForm'
 
 function HomePage () {
+  const handlePress = useCallback((value) => {
+    alert('press', value)
+  }, [])
+  const footer = useMemo(() => {
+    return <p>Footer</p>
+  }, [])
   return (
     <Page>
       <Header />
-      <SearchForm />
+      <SearchForm onPress={handlePress} />
+      {footer}
     </Page>
   )
 }
